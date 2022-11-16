afee6ff5bff44e27402aa4ca89e912f7128055df
KwonHeeHoon <kwonheehoon@gmail.com>
2022-11-16 13:39:06 +0900

    second commit

--- delivery/views.py
+++ delivery/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.utils import timezone
 
+
 @csrf_exempt
 def order_list(request):
     if request.method == 'GET':
